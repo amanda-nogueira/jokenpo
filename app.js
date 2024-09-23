280026724c08832ac25327721c5e6869e1dc9f03
amanda-nogueira <amanda.alves.n@hotmail.com>
2024-09-23 20:43:02 -0300

    jokenpo

---
+++ app.js
@@ -0,0 +1,39 @@
+/*
+@link https://github.com/amanda-nogueira/jokenpo.git
+@version 1.0
+*/
+function jogar() {
+    if (document.getElementById('pedra').checked === false &&
+        document.getElementById('papel').checked === false &&
+        document.getElementById('tesoura').checked === false) {
+        alert('Selecione uma opção')
+    } else {
+        let sorteio = Math.floor(Math.random() * 3)
+        switch (sorteio) {
+            case 0:
+                document.getElementById('pc').src = "/img/pcpedra.png"
+                break;
+            case 1:
+                document.getElementById('pc').src = "/img/pcpapel.png"
+                break;
+            case 2:
+                document.getElementById('pc').src = "/img/pctesoura.png"
+                break;
+        }
+        if ((document.getElementById('pedra').checked === true && sorteio === 0) ||
+            (document.getElementById('papel').checked === true && sorteio === 1) ||
+            (document.getElementById('tesoura').checked === true && sorteio === 2)) {
+            document.getElementById('placar').innerHTML = "Empate"
+        } else if ((document.getElementById('pedra').checked === true && sorteio === 2) ||
+            (document.getElementById('papel').checked === true && sorteio === 0) ||
+            (document.getElementById('tesoura').checked === true && sorteio === 1)) {
+            document.getElementById('placar').innerHTML = "Jogador venceu"
+        } else {
+            document.getElementById('placar').innerHTML = "Computador venceu"
+        }
+    }
+}
+function limpar() {
+    document.getElementById('pc').src = '/img/pc.png'
+    document.getElementById('placar').innerHTML = ""
+}
